6aa2a499f95ab713c38c947c85290dc621ae7e1f
surabhirandive <59006104+surabhirandive@users.noreply.github.com>
2020-09-08 14:07:00 +0530

    new fact

---
+++ Fatorial.java
@@ -0,0 +1,14 @@
+package in.co.start.javabasics;
+
+public class Fatorial {
+	public static void main(String[] args) {
+		int i=4;
+		int fact=1;
+		for(int j=1;j<=i;j++){
+			fact=fact*j;
+		}
+		System.out.println(fact);
+	}
+	
+
+}
